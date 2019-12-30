79df0e6c1682e0c3ac83304b2ea0d281d232dfc2
Guy Melul <guymalul95@gmail.com>
2019-12-30 22:39:31 +0200

    First commit

---
+++ src/main/java/io/guymelul/beesync/BeeSyncApplication.java
@@ -0,0 +1,13 @@
+package io.guymelul.beesync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeeSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeeSyncApplication.class, args);
+	}
+
+}
